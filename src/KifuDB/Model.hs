a049d3dafb358ff663632da44b1682d4a6b241a8
Shinichiro Suzuki <shinichiro.su@gmail.com>
2015-10-20 06:26:06 +0900

    Ban関係の関数をKifuDB.Model.Typeに移した

--- src/KifuDB/Model.hs
+++ src/KifuDB/Model.hs
@@ -20,8 +20,6 @@ module KifuDB.Model
     , getKyokumen
     , insertKifu
     , insertKyokumen
-    , P.toSqlKey
-    , P.entityVal
     , drawBan
     ) where
 
@@ -31,11 +29,9 @@ import           Control.Monad.Logger                  (NoLoggingT)
 import           Control.Monad.Trans.Control           (MonadBaseControl)
 import           Control.Monad.Trans.Resource.Internal (ResourceT)
 import qualified Data.Aeson                            as A
-import           Data.Foldable                         (foldl')
 import qualified Data.Map.Strict                       as M
 import           Data.Maybe                            (fromJust)
 import           Data.Text.Lazy                        (Text, append)
-import qualified Data.Text.Lazy
 import qualified Database.Persist                      as P
 import qualified Database.Persist.Sqlite               as P
 import           Database.Persist.TH
@@ -105,94 +101,3 @@ runDB query = liftIO $ P.runSqlite dbname $ do
 
 initKyokumen :: KifuId -> Kyokumen
 initKyokumen kifuId = Kyokumen kifuId 0 initBan Nothing [] [] Nothing
-
-initBan :: Ban
-initBan = Ban $ foldl' (\b (masu, pos) -> M.adjust (const masu) pos b) (ban emptyBan)
-          [ (Just (Masu Ky P2), posToKey $ Pos 1 1)
-          , (Just (Masu Ke P2), posToKey $ Pos 2 1)
-          , (Just (Masu Gn P2), posToKey $ Pos 3 1)
-          , (Just (Masu Kn P2), posToKey $ Pos 4 1)
-          , (Just (Masu Ou P2), posToKey $ Pos 5 1)
-          , (Just (Masu Kn P2), posToKey $ Pos 6 1)
-          , (Just (Masu Gn P2), posToKey $ Pos 7 1)
-          , (Just (Masu Ke P2), posToKey $ Pos 8 1)
-          , (Just (Masu Ky P2), posToKey $ Pos 9 1)
-          , (Just (Masu Kk P2), posToKey $ Pos 2 2)
-          , (Just (Masu Hi P2), posToKey $ Pos 8 2)
-          , (Just (Masu Fu P2), posToKey $ Pos 1 3)
-          , (Just (Masu Fu P2), posToKey $ Pos 2 3)
-          , (Just (Masu Fu P2), posToKey $ Pos 3 3)
-          , (Just (Masu Fu P2), posToKey $ Pos 4 3)
-          , (Just (Masu Fu P2), posToKey $ Pos 5 3)
-          , (Just (Masu Fu P2), posToKey $ Pos 6 3)
-          , (Just (Masu Fu P2), posToKey $ Pos 7 3)
-          , (Just (Masu Fu P2), posToKey $ Pos 8 3)
-          , (Just (Masu Fu P2), posToKey $ Pos 9 3)
-          , (Just (Masu Fu P1), posToKey $ Pos 1 7)
-          , (Just (Masu Fu P1), posToKey $ Pos 2 7)
-          , (Just (Masu Fu P1), posToKey $ Pos 3 7)
-          , (Just (Masu Fu P1), posToKey $ Pos 4 7)
-          , (Just (Masu Fu P1), posToKey $ Pos 5 7)
-          , (Just (Masu Fu P1), posToKey $ Pos 6 7)
-          , (Just (Masu Fu P1), posToKey $ Pos 7 7)
-          , (Just (Masu Fu P1), posToKey $ Pos 8 7)
-          , (Just (Masu Fu P1), posToKey $ Pos 9 7)
-          , (Just (Masu Hi P1), posToKey $ Pos 2 8)
-          , (Just (Masu Kk P1), posToKey $ Pos 8 8)
-          , (Just (Masu Ky P1), posToKey $ Pos 1 9)
-          , (Just (Masu Ke P1), posToKey $ Pos 2 9)
-          , (Just (Masu Gn P1), posToKey $ Pos 3 9)
-          , (Just (Masu Kn P1), posToKey $ Pos 4 9)
-          , (Just (Masu Ou P1), posToKey $ Pos 5 9)
-          , (Just (Masu Kn P1), posToKey $ Pos 6 9)
-          , (Just (Masu Gn P1), posToKey $ Pos 7 9)
-          , (Just (Masu Ke P1), posToKey $ Pos 8 9)
-          , (Just (Masu Ky P1), posToKey $ Pos 9 9)
-          ]
-
-
-emptyBan :: Ban
-emptyBan = Ban $ M.fromList $ zip (map posToKey possOnBan) (repeat Nothing)
-
-
-possOnBan :: [Pos]
-possOnBan = [Pos x y | x <- [1..9], y <- [1..9]]
-
-
-nextBan :: Ban -> Fugou -> Ban
-nextBan b f = changeTo (changeFrom b f) f
-  where
-    changeTo :: Ban -> Fugou -> Ban
-    changeTo b f = Ban $ M.update (\masu -> Just (Just (Masu (fugouKoma f) (fugouPlayer f)))) (posToKey (fugouTo f)) (ban b)
-
-    changeFrom :: Ban -> Fugou -> Ban
-    changeFrom b f = case fugouFrom f of
-      Nothing -> b
-      Just pos -> Ban $ M.update (const (Just Nothing)) (posToKey pos) (ban b)
-
-posToKey :: Pos -> String
-posToKey (Pos x y) = show x ++ show y
-
-posFromKey :: String -> Pos
-posFromKey key = Pos (posInt `div` 10) (posInt `mod` 10)
-  where
-    posInt :: Int
-    posInt = read key
-
-drawBan :: Ban -> Text
-drawBan (Ban b) =
-  Data.Text.Lazy.concat [
-      "<html><body><table>"
-    , Data.Text.Lazy.concat $ map ((\t -> Data.Text.Lazy.concat ["<tr>", t, "</tr>"]) . col) [1..9]
-    , "</table></body></html>"
-    ]
-  where
-    banList :: [((Int, Int), Text)]
-    banList = map (\(k, mMas) -> ((posX $ posFromKey k, posY $ posFromKey k), mMasuToText mMas)) $ M.toList b
-
-    mMasuToText :: Maybe Masu -> Text
-    mMasuToText (Just (Masu koma player)) = Data.Text.Lazy.concat["<td>", showKoma koma, if player == P1 then "^" else "v", "</td>"]
-    mMasuToText Nothing = "<td>　 </td>"
-
-    col :: Int -> Text
-    col y = Data.Text.Lazy.concat $ reverse $ map snd $ filter (\((_, y'), _) -> y' == y) banList
--- src/KifuDB/Model/Type.hs
+++ src/KifuDB/Model/Type.hs
@@ -11,15 +11,21 @@ module KifuDB.Model.Type
     , Ban(..)
     , Fugou(..)
     , showKoma
+    , posToKey
+    , posFromKey
+    , initBan
+    , emptyBan
+    , nextBan
+    , drawBan
       ) where
 
-import qualified Data.Aeson              as A
-import           Data.Int                (Int64)
-import qualified Data.Map.Strict         as M
-import           Data.Text.Lazy          (Text)
-import           Database.Persist
-import           Database.Persist.Sqlite
-import           Database.Persist.TH
+import qualified Data.Aeson          as A
+import           Data.Foldable       (foldl')
+import           Data.Int            (Int64)
+import qualified Data.Map.Strict     as M
+import           Data.Text.Lazy      (Text)
+import qualified Data.Text.Lazy
+import           Database.Persist.TH (derivePersistField)
 import           GHC.Generics
 
 data Player = P1 | P2 deriving (Show, Read, Eq, Generic)
@@ -96,3 +102,95 @@ instance A.FromJSON Fugou
 instance A.ToJSON Fugou
 
 derivePersistField "Fugou"
+
+posToKey :: Pos -> String
+posToKey (Pos x y) = show x ++ show y
+
+posFromKey :: String -> Pos
+posFromKey key = Pos (posInt `div` 10) (posInt `mod` 10)
+  where
+    posInt :: Int
+    posInt = read key
+
+
+initBan :: Ban
+initBan = Ban $ foldl' (\b (masu, pos) -> M.adjust (const masu) pos b) (ban emptyBan)
+          [ (Just (Masu Ky P2), posToKey $ Pos 1 1)
+          , (Just (Masu Ke P2), posToKey $ Pos 2 1)
+          , (Just (Masu Gn P2), posToKey $ Pos 3 1)
+          , (Just (Masu Kn P2), posToKey $ Pos 4 1)
+          , (Just (Masu Ou P2), posToKey $ Pos 5 1)
+          , (Just (Masu Kn P2), posToKey $ Pos 6 1)
+          , (Just (Masu Gn P2), posToKey $ Pos 7 1)
+          , (Just (Masu Ke P2), posToKey $ Pos 8 1)
+          , (Just (Masu Ky P2), posToKey $ Pos 9 1)
+          , (Just (Masu Kk P2), posToKey $ Pos 2 2)
+          , (Just (Masu Hi P2), posToKey $ Pos 8 2)
+          , (Just (Masu Fu P2), posToKey $ Pos 1 3)
+          , (Just (Masu Fu P2), posToKey $ Pos 2 3)
+          , (Just (Masu Fu P2), posToKey $ Pos 3 3)
+          , (Just (Masu Fu P2), posToKey $ Pos 4 3)
+          , (Just (Masu Fu P2), posToKey $ Pos 5 3)
+          , (Just (Masu Fu P2), posToKey $ Pos 6 3)
+          , (Just (Masu Fu P2), posToKey $ Pos 7 3)
+          , (Just (Masu Fu P2), posToKey $ Pos 8 3)
+          , (Just (Masu Fu P2), posToKey $ Pos 9 3)
+          , (Just (Masu Fu P1), posToKey $ Pos 1 7)
+          , (Just (Masu Fu P1), posToKey $ Pos 2 7)
+          , (Just (Masu Fu P1), posToKey $ Pos 3 7)
+          , (Just (Masu Fu P1), posToKey $ Pos 4 7)
+          , (Just (Masu Fu P1), posToKey $ Pos 5 7)
+          , (Just (Masu Fu P1), posToKey $ Pos 6 7)
+          , (Just (Masu Fu P1), posToKey $ Pos 7 7)
+          , (Just (Masu Fu P1), posToKey $ Pos 8 7)
+          , (Just (Masu Fu P1), posToKey $ Pos 9 7)
+          , (Just (Masu Hi P1), posToKey $ Pos 2 8)
+          , (Just (Masu Kk P1), posToKey $ Pos 8 8)
+          , (Just (Masu Ky P1), posToKey $ Pos 1 9)
+          , (Just (Masu Ke P1), posToKey $ Pos 2 9)
+          , (Just (Masu Gn P1), posToKey $ Pos 3 9)
+          , (Just (Masu Kn P1), posToKey $ Pos 4 9)
+          , (Just (Masu Ou P1), posToKey $ Pos 5 9)
+          , (Just (Masu Kn P1), posToKey $ Pos 6 9)
+          , (Just (Masu Gn P1), posToKey $ Pos 7 9)
+          , (Just (Masu Ke P1), posToKey $ Pos 8 9)
+          , (Just (Masu Ky P1), posToKey $ Pos 9 9)
+          ]
+
+
+emptyBan :: Ban
+emptyBan = Ban $ M.fromList $ zip (map posToKey possOnBan) (repeat Nothing)
+
+
+possOnBan :: [Pos]
+possOnBan = [Pos x y | x <- [1..9], y <- [1..9]]
+
+
+nextBan :: Ban -> Fugou -> Ban
+nextBan b f = changeTo (changeFrom b f) f
+  where
+    changeTo :: Ban -> Fugou -> Ban
+    changeTo b f = Ban $ M.update (\masu -> Just (Just (Masu (fugouKoma f) (fugouPlayer f)))) (posToKey (fugouTo f)) (ban b)
+
+    changeFrom :: Ban -> Fugou -> Ban
+    changeFrom b f = case fugouFrom f of
+      Nothing -> b
+      Just pos -> Ban $ M.update (const (Just Nothing)) (posToKey pos) (ban b)
+
+drawBan :: Ban -> Text
+drawBan (Ban b) =
+  Data.Text.Lazy.concat [
+      "<html><body><table>"
+    , Data.Text.Lazy.concat $ map ((\t -> Data.Text.Lazy.concat ["<tr>", t, "</tr>"]) . col) [1..9]
+    , "</table></body></html>"
+    ]
+  where
+    banList :: [((Int, Int), Text)]
+    banList = map (\(k, mMas) -> ((posX $ posFromKey k, posY $ posFromKey k), mMasuToText mMas)) $ M.toList b
+
+    mMasuToText :: Maybe Masu -> Text
+    mMasuToText (Just (Masu koma player)) = Data.Text.Lazy.concat["<td>", showKoma koma, if player == P1 then "^" else "v", "</td>"]
+    mMasuToText Nothing = "<td>　 </td>"
+
+    col :: Int -> Text
+    col y = Data.Text.Lazy.concat $ reverse $ map snd $ filter (\((_, y'), _) -> y' == y) banList
